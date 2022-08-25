24db9fdf2b965cc81483aaf6b163652cccbdd3d0
ATG128 <91274939+ATG128@users.noreply.github.com>
2022-08-25 16:07:02 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,35 @@
+
+/**
+ * 
+ *
+ *
+ *
+ */
+public class nwiggy
+{
+    static int x=0;
+    static int[] arr1= new int[x];
+    static String[] sc= new String[x];
+    static int[] rate= new int [x];
+    
+    public static void main ()
+    {
+        
+        {
+            System.out .println("$$\\   $$\\               $$\\                               ");
+            System.out .println("$$$\\  $$ |              \\__|                              ");
+            System.out .println("$$$$\\ $$ |$$\\  $$\\  $$\\ $$\\  $$$$$$\\   $$$$$$\\  $$\\   $$\\ ");
+            System.out .println("$$ $$\\$$ |$$ | $$ | $$ |$$ |$$  __$$\\ $$  __$$\\ $$ |  $$ |");
+            System.out .println("$$ \\$$$$ |$$ | $$ | $$ |$$ |$$ /  $$ |$$ /  $$ |$$ |  $$ |");
+            System.out .println("$$ |\\$$$ |$$ | $$ | $$ |$$ |$$ |  $$ |$$ |  $$ |$$ |  $$ |");
+            System.out .println("$$ | \\$$ |\\$$$$$\\$$$$  |$$ |\\$$$$$$$ |\\$$$$$$$ |\\$$$$$$$ |");
+            System.out .println("\\__|  \\__| \\_____\\____/ \\__| \\____$$ | \\____$$ | \\____$$ |");
+            System.out .println("                            $$\\   $$ |$$\\   $$ |$$\\   $$ |");
+            System.out .println("                            \\$$$$$$  |\\$$$$$$  |\\$$$$$$  |");
+            System.out .println("                             \\______/  \\______/  \\______/ ");
+            
+        }
+        
+    }
+    
+}
